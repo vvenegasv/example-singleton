c91b3b50fa774a3aaee98c7564f9e46d6b8ea9f7
Vladimir Venegas <vvenegas@infodinamica.cl>
2017-04-05 11:48:14 -0300

    Se crea ejemplo simple de singleton
    Se podría utilizar en firehorse, dado que actualmente está ocupando una
    clase estática

---
+++ src/ExampleSingleton/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ExampleSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instance1 = SingletonClass.Instance;
+            var instance2 = SingletonClass.Instance;
+
+            if (instance1.GetKey() != instance2.GetKey())
+                WriteLine($"La clave {instance1.GetKey()} es distinta a {instance2.GetKey()} ");
+            else
+                WriteLine("Las claves son iguales");
+
+            if (instance1 != instance2)
+                WriteLine("Las instancias son distintas");
+            else
+                WriteLine("Las instancias son iguales");
+
+            ReadKey();
+        }
+    }
+}
